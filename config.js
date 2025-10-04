d3d92f8aa7a64d16cd00637b2cda9e3a08b9f12c
mkajla662-cloud <mkajla662@gmail.com>
2025-10-04 22:43:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const parsedId = Number(process.env.ID);
 module.exports = {
-  token: process.env.TOKEN || '8364844663:AAEQI_q5QmfYEoWdCoh807Hz0dItaChozBI',
+  token: process.env.TOKEN || '7981839991:AAE2SS2__cWThf2C9bTzhWtWDzCk8c66bjk',
   id: isNaN(parsedId) ? 7167916864 : parsedId // replace 12345.. with your telegram chat id
 };
